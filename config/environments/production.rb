9ade9c57813c0765556f99b29223e900fc3e46fe
Maeka Shiroyama <shiroyama.maeka@gmail.com>
2018-10-26 12:14:35 +0000

    Mailer設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.action_mailer.default_url_options = { host: 'guarded-meadow-86767' }
+  config.action_mailer.default_url_options = { host: 'guarded-meadow-86767.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
